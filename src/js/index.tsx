9784e94766402ce85ecb16fa9ea833d9fa045f2d
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2023-03-22 21:11:01 +0100

    fix lint issue and update gitignore

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('app')!;
-const root = createRoot(container);
-root.render(<App />);
+const container = document.getElementById('app');
+if (container) {
+	const root = createRoot(container);
+	root.render(<App />);
+}
